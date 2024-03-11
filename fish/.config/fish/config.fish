c4751870a9dc9761a0c76ceaf467269b39b1fdd6
asiryk <asiryk.net@gmail.com>
2024-03-11 19:08:54 +0200

    fix variables

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,42 +2,43 @@
 # In order to make it default shell on mac, add entry to /etc/shells
 #
 
+# Variables
+set -x ZDOTDIR $HOME/.config/zsh
+set -x XDG_DATA_HOME $HOME/.local/share
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x XDG_CACHE_HOME $HOME/.cache
+set -x MANPAGER "nvim +Man!"
+set -x EDITOR "nvim"
+set -x NVIMDIR $HOME/.config/nvim
+set -x FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
+set -x GIT_DISCOVERY_ACROSS_FILESYSTEM 1
+
+# Path
+fish_add_path ~/bin
+fish_add_path /opt/homebrew/bin
+fish_add_path /Users/asiryk/Library/Python/3.9/bin
+fish_add_path "/Applications/IntelliJ IDEA.app/Contents/MacOS"
+fish_add_path $HOME/.cargo/bin
+
+set -g fish_color_cwd cyan
+set -g fish_color_command green
+set -g fish_color_param white
+
+# Programs
+zoxide init fish | source
+fnm env --use-on-cd | source
+
+# Aliases
+alias gs='git status'
+alias ll='ls -al'
+alias vim="nvim --noplugin"
+alias vi="nvim --clean"
+alias v="vi"
 
 function fish_user_key_bindings
-    # Variables
-    set -x ZDOTDIR=$HOME/.config/zsh
-    set -x XDG_DATA_HOME=$HOME/.local/share
-    set -x XDG_CONFIG_HOME=$HOME/.config
-    set -x XDG_CACHE_HOME=$HOME/.cache
-    set -x MANPAGER="nvim +Man!"
-    set -x EDITOR="nvim"
-    set -x NVIMDIR=$HOME/.config/nvim
-    set -x FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
-    set -x GIT_DISCOVERY_ACROSS_FILESYSTEM=1
-
-    # Path
-    fish_add_path ~/bin
-    fish_add_path /opt/homebrew/bin
-    fish_add_path /Users/asiryk/Library/Python/3.9/bin
-    fish_add_path /Applications/IntelliJ IDEA.app/Contents/MacOS
-    fish_add_path $HOME/.cargo/bin
-
     # Enable vim mode
     fish_default_key_bindings -M insert
     fish_vi_key_bindings --no-erase insert
-
-    theme_colors
-
-    # Programs
-    zoxide init fish | source
-    fnm env --use-on-cd | source
-
-    # Aliases
-    alias gs='git status'
-    alias ll='ls -al'
-    alias vim="nvim --noplugin"
-    alias vi="nvim --clean"
-    alias v="vi"
 end
 
 function fish_prompt --description 'minimum info snappy prompt'
@@ -68,10 +69,4 @@ function fish_mode_prompt --description 'set cursor style depending on vim mode'
         case replace_one
             echo -n -e "\e[4 q"  # Underscore Shape
     end
-end
-
-function theme_colors --description 'set custome theme colors'
-    set -g fish_color_cwd cyan
-    set -g fish_color_command green
-    set -g fish_color_param white
 end
